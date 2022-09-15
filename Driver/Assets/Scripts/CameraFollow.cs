8c2ea442ba8ef3d6e0ef14337f97716265902573
Murat Özdemir <muratozdmrcn75@gmail.com>
2022-09-15 15:20:46 +0300

    2.0
    Araba Hareket Bitti

---
+++ Driver/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+    public Vector3 offset;
+
+
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void FixedUpdate()
+    {
+        transform.position = Vector3.Lerp(transform.position, target.position + offset, Time.deltaTime * 10);
+    }
+}
---
+++ Driver/Assets/Scripts/CarController.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    [SerializeField] float carSpeed;
+    [SerializeField] float maxSpeed;
+    [SerializeField] float steerAngle;
+    public Transform lw, rw;
+    private float dragAmount = 0.91f;
+    Vector3 rotVec;
+    Vector3 moveVec;
+
+
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void FixedUpdate()
+    {
+        moveVec += transform.forward * carSpeed * Time.deltaTime;
+        transform.position += moveVec * Time.deltaTime; // "transform.translate +=" de kullanılabilir.
+
+        rotVec += new Vector3(0, Input.GetAxis("Horizontal"), 0);
+
+        transform.Rotate(Vector3.up * Input.GetAxis("Horizontal") * steerAngle * moveVec.magnitude * Time.deltaTime);
+
+        moveVec *= dragAmount;
+        moveVec = Vector3.ClampMagnitude(moveVec, maxSpeed);
+        rotVec = Vector3.ClampMagnitude(rotVec, steerAngle);
+
+        lw.localRotation = Quaternion.Euler(rotVec);
+        rw.localRotation = Quaternion.Euler(rotVec);
+    }
+}
