7094c4a4ca8829b8106a8edc208877bd4d2fc410
Murat Özdemir <muratozdmrcn75@gmail.com>
2022-09-23 14:14:40 +0300

    Hata3

--- Driver/Assets/Scripts/CameraFollow.cs
+++ Driver/Assets/Scripts/CameraFollow.cs
@@ -4,19 +4,29 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    public Transform target;
-    public Vector3 offset;
+    [SerializeField] private Transform target;
+    [SerializeField] private Vector3 offset;
+    [SerializeField] private float translateSpeed;
+    [SerializeField] private float rotationSpeed;
 
 
-    
-    void Start()
+    private void LateUpdate()
     {
-        
+        moveTheCamera();
     }
 
-    
-    void FixedUpdate()
+
+    private void moveTheCamera()
     {
-        transform.position = Vector3.Lerp(transform.position, target.position + offset, Time.deltaTime * 10);
+        var targetPosition = target.TransformPoint(offset);
+        transform.position = Vector3.Lerp(transform.position, targetPosition, translateSpeed * Time.deltaTime);
+    }
+
+
+    private void rotateTheCamera()
+    {
+        var direction = target.position - transform.position;
+        var rotation = Quaternion.LookRotation(direction, Vector3.up);
+        transform.rotation = Quaternion.Lerp(transform.rotation, rotation, rotationSpeed * Time.deltaTime);
     }
 }
